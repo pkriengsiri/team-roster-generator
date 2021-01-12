ff0514119172e3dadd90fe98e0743311b53c2668
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-12 18:15:06 -0500

    Added Manager class constructor to Manager.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,13 +1,13 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
-xtest("Can set office number via constructor argument", () => {
+test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
-xtest('getRole() should return "Manager"', () => {
+test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
   expect(e.getRole()).toBe(testValue);
