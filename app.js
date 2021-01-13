feea20eb9190239b72f919e4a327df2bf28d9f56
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-12 21:19:23 -0500

    Added renderHTML() method to Roster class

--- app.js
+++ app.js
@@ -35,5 +35,6 @@ const render = require("./lib/htmlRenderer");
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
+
 const myRoster = new Roster();
 myRoster.init();
